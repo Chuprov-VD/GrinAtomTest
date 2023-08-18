1b4624969ccdb55e72fd6685d581baaa5eba6370
chuprov-vd <chuprov271@gmail.com>
2023-08-19 00:18:27 +0300

    Task correction

--- Task1.java
+++ Task1.java
@@ -2,21 +2,19 @@ package TaskAtomGrin;
 
 public class Task1 {
         public static void main(String[] args) {
-            int x = 3;
-            int y = 5;
-            String a = "Питер";
-            String b = "Москва";
+            int a = 3;
+            int b = 5;
             ExchangeStr(b, a); // первый способ
 
-            x = x + y;  // второй способ
-            y = x - y;
-            x = x - y;
-            System.out.printf("\nx = %s", x);
-            System.out.printf("\ny = %s", y);
+            a = a + b;  // второй способ
+            b = a - b;
+            a = a - b;
+            System.out.printf("\na = %s", a);
+            System.out.printf("\nb = %s", b);
 
 
         }
-        static void ExchangeStr(String a, String b){
+        static void ExchangeStr(int a, int b){
             System.out.printf("a = %s", a);
             System.out.printf("\nb = %s", b);
         }
